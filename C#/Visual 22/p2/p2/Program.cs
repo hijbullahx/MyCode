b60e5b6a4d75cb9755ce9a0fa99990d7ac3f8836
MD TAHER BIN OMAR HIJBULLAH <hijbullah119445@gmail.com>
2025-03-10 12:07:36 +0600

    Added CSp project

---
+++ C#/Visual 22/p2/p2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string name = "Hijbullah";
+        Console.WriteLine("Hello, " + name);
+        name = "Naeim";
+        Console.WriteLine("Hello, " + name);
+    }
+}
---
+++ C#/first.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace FirstProgram
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Please enter something:");
+      string input = Console.ReadLine();
+      Console.WriteLine("You entered: " + input);
+    }
+  }
+}
