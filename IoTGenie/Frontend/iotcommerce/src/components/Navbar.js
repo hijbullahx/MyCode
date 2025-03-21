6f8f41898407cdf53ff52b418487c176d46ab197
MD TAHER BIN OMAR HIJBULLAH <hijbullah119445@gmail.com>
2025-03-21 18:42:33 +0600

    Auto commit from CodeVault

--- IoTGenie/Frontend/iotcommerce/src/components/Navbar.js
+++ IoTGenie/Frontend/iotcommerce/src/components/Navbar.js
@@ -1,50 +1,82 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { LinkContainer } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 function Navbar() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">IoTGenie</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarColor01">
-      <ul className="navbar-nav me-auto">
-        <li className="nav-item">
-          <a className="nav-link active" href="#">Home
-            <span className="visually-hidden">(current)</span>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-          <div className="dropdown-menu">
-            <a className="dropdown-item" href="#">Action</a>
-            <a className="dropdown-item" href="#">Another action</a>
-            <a className="dropdown-item" href="#">Something else here</a>
-            <div className="dropdown-divider"></div>
-            <a className="dropdown-item" href="#">Separated link</a>
+      <Navbar
+        className="navbar navbar-expand-lg bg-primary"
+        data-bs-theme="dark"
+      >
+        <div className="container-fluid">
+          <LinkContainer to="/">
+            <Nav.Link className="navbar-brand">IoTGenie</Nav.Link>
+          </LinkContainer>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarColor01"
+            aria-controls="navbarColor01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  Home
+                  <span className="visually-hidden">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Cart
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                  href="#"
+                  role="button"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  New User
+                </a>
+                <div className="dropdown-menu">
+                  <a className="dropdown-item" href="#">
+                    Login
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Signup
+                  </a>
+                  <div className="dropdown-divider"></div>
+                  <a className="dropdown-item" href="#">
+                    Logout
+                  </a>
+                </div>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input
+                className="form-control me-sm-2"
+                type="search"
+                placeholder="Search"
+              />
+              <button className="btn btn-secondary my-2 my-sm-0" type="submit">
+                Search
+              </button>
+            </form>
           </div>
-        </li>
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-sm-2" type="search" placeholder="Search" />
-        <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+        </div>
+      </Navbar>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
