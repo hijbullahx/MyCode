49c834c31a1d34d836709554d40935128be1ffb4
MD TAHER BIN OMAR HIJBULLAH <hijbullah119445@gmail.com>
2025-03-24 12:26:52 +0600

    Auto commit from CodeVault

---
+++ C#/CSC 439/Abstract class/Abstract class/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Abstract
+{
+    abstract class Shape
+    {
+        public abstract double CalculateArea();
+        public void Display()
+        {
+            Console.WriteLine("Calculating area...");
+        }
+    }
+    class Circle : Shape
+    {
+        private double radius;
+        public Circle(double r)
+        {
+            radius = r;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+    class Rectangle : Shape
+    {
+        private double length, width;
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+        public override double CalculateArea()
+        {
+            return length * width;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Shape circle = new Circle(5);
+            circle.Display();
+            Console.WriteLine("Circle Area: " + circle.CalculateArea());
+            Shape rectangle = new Rectangle(4, 6);
+            rectangle.Display();
+            Console.WriteLine("Rectangle Area: " + rectangle.CalculateArea());
+        }
+    }
+}
